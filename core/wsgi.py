a698bc7e33ad24a564920f494161e83615350fb8
amalazza <dinaazza8@gmail.com>
2021-05-06 05:46:56 +0700

    initial commit

---
+++ core/wsgi.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
+
